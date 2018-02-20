b51ffddf4d5a540ba3c07fdb474117a36ae46dcc
AlexFursenkoOd <andersen.od.ua@gmail.com>
2018-02-20 16:20:16 +0200

    Algorithm for finding Natural Divisors for given number

---
+++ NaturalDivisors/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NaturalDivisors
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var devisors = GetNaturalDivisors(54);
+            PrintCollection(devisors);
+            Console.ReadKey();
+        }
+
+        static List<int> GetNaturalDivisors(int number)
+        {
+            var devisors = new List<int>();
+            var root = Math.Sqrt(number);
+            var devisor = 1;
+
+            while(devisor < root)
+            {
+                var rest = number % devisor;
+                var result = number / devisor;
+                if(rest == 0)
+                {
+                    devisors.Add(devisor);
+                    if (devisor != result)
+                        devisors.Add(result);
+                }
+                devisor++;
+            }
+            return devisors;
+        }
+
+        static void PrintCollection<T>(IEnumerable<T> collection)
+        {
+            foreach(var item in collection)
+            {
+                Console.Write(item.ToString() + ", ");
+            }
+        }
+    }
+}
